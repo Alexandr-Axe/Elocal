083dbf5716576436c018269d15be3c44bb5a3c4d
Sekera-Alexandr <120587451+Sekera-Alexandr@users.noreply.github.com>
2026-04-27 14:57:21 +0200

    calculating k factor

--- Elocal/calculator.py
+++ Elocal/calculator.py
@@ -1,4 +1,5 @@
 import math
+from datetime import date
 
 def calculate_win_likelihood(player_rating, opponent_rating):
     """
@@ -58,7 +59,26 @@ def calculate_k_factor(player_rating, first_tournament_date):
     Returns:
     int: The K-factor for the player.
     """
-    return 40
+    today = date.today()
+
+    if first_tournament_date is None:
+        return 0
+
+    if player_rating >= 2400:
+        return 24
+
+    days_since_last_tournament = (today - first_tournament_date).days if first_tournament_date else None
+    years_since_last_tournament = days_since_last_tournament // 365
+
+    if years_since_last_tournament <= 0:
+        return 40
+    elif years_since_last_tournament == 1:
+        return 39
+    elif years_since_last_tournament == 2:
+        return 38
+    else:
+        k = 38 - 2 * (years_since_last_tournament - 2)
+        return max(24, k)
 
 def calculate_new_elo(player_rating, opponent_rating, index, k_factor = 40):
     """
